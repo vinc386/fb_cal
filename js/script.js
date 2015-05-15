116b6f9bb752a1c60729d8f574412b0238666629
Vincent Y <yusyang686@gmail.com>
2015-05-15 01:38:24 -0400

    calculate duration, overlapping(WIP)

--- js/script.js
+++ js/script.js
@@ -1,17 +1,52 @@
 (function (w, $, _) {
     'use strict';
-    var eventArr = [{start: 30, end: 150}, {start: 540, end: 600},
-        {start: 560, end: 620}, {start: 610, end: 670} ];
+    var eventArr;
     var timestampTmpl = getTempl('#timestampTmpl');
     var timelineTmpl = getTempl('#timelineTmpl');
 
     w.layOutDay = function (events) {
+        eventArr = _.map(events, getEventDuration);
+        $('.timelineWrapper').html(renderTimeline(9, 21));
+        getOverlappedEventCount(eventArr);
         console.log('blah', events);
     };
 
     function getTempl (selector) {
         return _.template($(selector).html(), {variable: 'data'});
     }
+
+    function getEventDuration (e) {
+        return _.defaults({}, e, {duration: e.end - e.start})
+    }
+
+    function getOverlappedEventCount (eventList) {
+        var rest;
+        var isOverlapped = function (e1, e2) {
+            if (
+                // 1 event starts in the middle of another event
+                // 2 events start the same time
+                // 2 events start and end at the same time
+                (e1.start <= e2.start && e1.end <= e2.start) ||
+                (e1.start >= e2.start && e1.end >= e2.end)
+                ) {
+                return true;
+            }
+            return false;
+        }
+
+        eventList.forEach(function (e) {
+            rest = _.without(eventList, e);
+            e.overlap = e.overlap || 0;
+
+            rest.forEach(function (_e) {
+                if (isOverlapped(e, _e))
+                {
+                    e.overlap += 1;
+                }
+            });
+        });
+        console.log(eventList);
+    };
 
     function generateTimeStamps (start, end) {
         var arr = [];
@@ -68,7 +103,7 @@
     }
 
     $(document).ready(function () {
-        $('.timelineWrapper').html(renderTimeline(9, 21));
-        w.layOutDay(eventArr);
+        w.layOutDay([{start: 30, end: 150}, {start: 540, end: 600},
+        {start: 560, end: 620}, {start: 610, end: 670} ]);
     });
 })(window, window.jQuery, window._);
