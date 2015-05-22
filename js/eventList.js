dd06b222a4ca8c873259d929963bad0ef15d5d2c
Vincent Y <yusyang686@gmail.com>
2015-05-22 01:40:16 -0400

    check overlapping recursively

--- js/eventList.js
+++ js/eventList.js
@@ -1,16 +1,15 @@
 (function (w, CalendarEvent, _) {
     'use strict';
     var eventList;
-    var totalWidth;
 
-    function EventList (collection, wrapperWidth) {
+    function EventList (collection) {
         eventList = _.map(collection, instantiateEvent);
-        totalWidth = wrapperWidth;
         _.sortBy(eventList, 'start');
         getOverlappedEventCount();
         eventList.forEach(getOffsets);
         return _.extend([], this, eventList);
     }
+
 
     function instantiateEvent (obj, index) {
         return new CalendarEvent(obj, index);
@@ -18,46 +17,59 @@
 
     function getOverlappedEventCount() {
         eventList.forEach(function (_event, index) {
-            checkOverlappedWithAllPreviousEvents(_event, null, index);
+            var overlapped = checkOverlap(_event, getPrevEvent(_event));
         });
     }
 
-    function checkOverlappedWithAllPreviousEvents (_event, prevEvent, index) {
-        var _prevEvent;
-        _event.prevEvent = (index && index > 0) ? eventList[index - 1] : null;
-        _prevEvent = prevEvent || _event.prevEvent;
-
-        if (_prevEvent && _event.isOverlappedWith(_prevEvent)) {
-            _event.overlappedWith.push(_prevEvent);
-            _prevEvent.overlappedWith.push(_event);
-            if(_prevEvent.prevEvent !== null) {
-                checkOverlappedWithAllPreviousEvents(_event, _prevEvent.prevEvent, index);
-            // } else {
-
-            }
+    function getPrevEvent(_event) {
+        var index = eventList.indexOf(_event);
+        if (index > 0) {
+            return eventList[index - 1];
+        } else if (index === 0) {
+            return eventList[eventList.length - 1];
+        } else {
+            return false;
         }
     }
 
-    function getOffsets (_event, index) {
-        var overlappedArr = _event.overlappedWith;
-        var _width;
+    function checkOverlap(_event, prevEvent) {
+        var result = {};
+        var temp;
 
-        if (!overlappedArr.length) {
-            _event.setDisplayAttr({
-                left: '15px',
-                width: totalWidth + 'px'
-            });
+        if (_event.isOverlappedWith(prevEvent)) {
+            result._left = prevEvent;
+            prevEvent.overlappedWith.push(_event);
+            _event.overlappedWith.push(prevEvent);
+
+            if(prevEvent.onLeft) {
+                if (!_event.isOverlappedWith(prevEvent.onLeft)) {
+                    temp = checkOverlap(_event, prevEvent.onLeft);
+                    result._left = temp._left;
+                    result._right = prevEvent.onLeft.onLeft ? temp._right : prevEvent;
+                } else {
+                    _event.overlappedWith.push(prevEvent.onLeft);
+                    prevEvent.onLeft.overlappedWith.push(_event);
+                }
+            }
         } else {
-            _width = totalWidth / (overlappedArr.length + 1);
-            console.log(_event.getAllPreviousOverlappedEvent());
-            _event.setDisplayAttr({
-                width: _width + 'px',
-                left: 15 + _width * overlappedArr.length + 'px'
-            });
-            // overlappedArr.forEach(function (__event, _index) {
-            // });
+            if (prevEvent.onLeft) {
+                temp = checkOverlap(_event, prevEvent.onLeft);
+                result._left = temp._left;
+                result._right = temp._right;
+            }
         }
-        console.log(_event);
+
+        _event.onRight = result._right;
+        _event.onLeft = result._left;
+        return result;
+    }
+
+    function getOffsets (_event, index) {
+        return _event.setDisplayAttr();
+        // _event.setDisplayAttr({
+        //     left: '15px',
+        //     width: totalWidth + 'px'
+        // });
     }
 
     w.EventList = EventList;
